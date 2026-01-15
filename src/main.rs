fa4eef0cdac9fde840e54bc855a0b8ecf7905fea
Sasha <sasha@Sashas-MacBook-Air.local>
2026-01-15 23:00:12 +0200

    Now drawing into a file so the image of a sphere drawn by a ray trace algorithm could be seen

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,22 @@ use std::ops::Add;
 use std::ops::Mul;
 use std::ops::Sub;
 
+const IMAGE_WIDTH: i32 = 400;
+const IMAGE_HEIGHT: i32 = 300;
+
+const SPHERE:Sphere = Sphere {
+    center: Vec3 { x: 0.0, y: 0.0, z: -2.0 },
+    radius: 0.5,
+};
+
+const VIEWPORT_HEIGHT: f32 = 2.0;
+const VIEWPORT_WIDTH: f32 = (IMAGE_WIDTH as f32 / IMAGE_HEIGHT as f32) * VIEWPORT_HEIGHT;
+const FOCAL_LENGTH: f32 = 1.0; // basically FOV
+
+const ORIGIN: Vec3 = Vec3 { x: 0.0, y: 0.5, z: 0.0 };
+const HORIZONTAL: Vec3 = Vec3 { x: VIEWPORT_WIDTH, y: 0.0, z: 0.0 };
+const VERTICAL: Vec3 = Vec3 { x: 0.0, y: VIEWPORT_HEIGHT, z: 0.0 };
+
 #[derive(Copy, Clone)]
 struct Vec3 {
     x: f32,
@@ -16,6 +32,15 @@ impl Vec3 {
 
     fn length_squared(&self) -> f32 {
         self.dot(*self)
+    }
+
+    fn length(&self) ->f32 {
+        self.length_squared().sqrt()
+    }
+
+    fn normalize(&self) -> Vec3 {
+        let len = self.length();
+        if len == 0.0 { *self } else { *self * (1.0 / len)}
     }
 }
 
@@ -40,6 +65,14 @@ impl Mul<f32> for Vec3 {
             y: self.y * rhs,
             z: self.z * rhs,
         }
+    }
+}
+
+impl Mul<Vec3> for f32 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        rhs * self
     }
 }
 
@@ -95,8 +128,46 @@ fn ray_trace(ray: &Ray, sphere: &Sphere) -> RayTraceResult {
     })
 }
 
+fn ray_color (ray: &Ray, sphere: &Sphere) -> Vec3 {
+    match ray_trace(ray, sphere) {
+        RayTraceResult::Hit(rec) => {
+            0.5 * (rec.hit_normal + Vec3 { x: 1.0, y: 1.0, z: 1.0}) // Mapping normal to [0, 1]
+        }
+        RayTraceResult::Miss => {
+            let unit_direction = ray.direction.normalize();
+            let t = 0.5 * (unit_direction.y + 1.0); // Mapping distance to [0, 1]
+            (1.0 - t) * Vec3 { x: 1.0, y: 1.0, z: 1.0} + t * Vec3{x: 0.5, y: 0.7, z: 1.0 } // lerp between white and blue
+        }
+    }
+}
+
 fn main() {
-    // Use ```cargo test```
+    let lower_left_corner = ORIGIN - HORIZONTAL * 0.5 - VERTICAL * 0.5 - Vec3 { x: 0.0, y: 0.0, z: FOCAL_LENGTH };
+
+    // FORMAT, WIDTH, HEIGHT, MAX_RGB 
+    println!("P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n 255");
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+
+        for i in 0..IMAGE_WIDTH {
+            // UV [0,1]
+            let u = i as f32 / (IMAGE_WIDTH - 1) as f32;
+            let v = j as f32 / (IMAGE_HEIGHT - 1) as f32;
+
+            let ray = Ray {
+                origin: ORIGIN,
+                direction: lower_left_corner + u * HORIZONTAL + v * VERTICAL - ORIGIN,
+            };
+
+            let pixel_color = ray_color(&ray, &SPHERE);
+
+            let r = (255.999 * pixel_color.x) as i32;
+            let g = (255.999 * pixel_color.y) as i32;
+            let b = (255.999 * pixel_color.z) as i32;
+
+            println!("{r} {g} {b}");
+        }
+    }
 }
 
 #[cfg(test)]
