98fba8562cc60537864bcaa5ba01514ab97a1e95
Sasha <sasha@Sashas-MacBook-Air.local>
2026-01-29 16:24:13 +0200

    Refactor for readability

--- src/main.rs
+++ src/main.rs
@@ -14,25 +14,25 @@ use crate::hittable::HittableList;
 
 const IMAGE_WIDTH: i32 = 1920;
 const IMAGE_HEIGHT: i32 = 1080;
-const SAMPLES: i32 = 10;
-const DIFFUSE_DEPTH: u32 = 50;
+const SAMPLES_PER_PIXEL: i32 = 10;
+const MAX_DEPTH: u32 = 50;
 
 // Camera params
 const VIEWPORT_HEIGHT: f32 = 2.0;
 const VIEWPORT_WIDTH: f32 = (IMAGE_WIDTH as f32 / IMAGE_HEIGHT as f32) * VIEWPORT_HEIGHT;
 const FOCAL_LENGTH: f32 = 1.0; // basically FOV
 
-const ORIGIN: Vec3 = Vec3 {
+const CAM_ORIGIN: Vec3 = Vec3 {
     x: 0.0,
     y: 0.5,
     z: 0.0,
 };
-const HORIZONTAL: Vec3 = Vec3 {
+const VIEWPORT_HORIZONTAL: Vec3 = Vec3 {
     x: VIEWPORT_WIDTH,
     y: 0.0,
     z: 0.0,
 };
-const VERTICAL: Vec3 = Vec3 {
+const VIEWPORT_VERTICAL: Vec3 = Vec3 {
     x: 0.0,
     y: VIEWPORT_HEIGHT,
     z: 0.0,
@@ -78,6 +78,16 @@ fn ray_color(world: &HittableList, ray: &Ray, depth: u32, rng: &mut impl Rng) ->
     }
 }
 
+fn translate_to_rgb_color(sampled_color: &mut Vec3) -> (i32, i32, i32) {
+    sampled_color.x = sampled_color.x.sqrt();
+    sampled_color.y = sampled_color.y.sqrt();
+    sampled_color.z = sampled_color.z.sqrt();
+    let r = (255.999 * sampled_color.x) as i32;
+    let g = (255.999 * sampled_color.y) as i32;
+    let b = (255.999 * sampled_color.z) as i32;
+    (r, g, b)
+}
+
 fn main() {
     let mut world = HittableList {
         objects: Vec::new(),
@@ -101,9 +111,9 @@ fn main() {
         radius: 100.0,
     }));
 
-    let lower_left_corner = ORIGIN
-        - HORIZONTAL * 0.5
-        - VERTICAL * 0.5
+    let lower_left_corner = CAM_ORIGIN
+        - VIEWPORT_HORIZONTAL * 0.5
+        - VIEWPORT_VERTICAL * 0.5
         - Vec3 {
             x: 0.0,
             y: 0.0,
@@ -118,7 +128,7 @@ fn main() {
     for j in (0..IMAGE_HEIGHT).rev() {
         for i in 0..IMAGE_WIDTH {
             let mut sampled_color: Vec3 = Vec3{ x: 0.0, y: 0.0, z: 0.0}; 
-            for k in 0..SAMPLES {
+            for _k in 0..SAMPLES_PER_PIXEL {
                 let offset_x: f32 = rng.gen_range(0.0..1.0);
                 let offset_y: f32 = rng.gen_range(0.0..1.0);
                 // UV [0,1]
@@ -126,22 +136,16 @@ fn main() {
                 let v = (j as f32 + offset_y) / (IMAGE_HEIGHT - 1) as f32;
                 
                 let ray = Ray {
-                    origin: ORIGIN,
-                    direction: (lower_left_corner + u * HORIZONTAL + v * VERTICAL - ORIGIN).normalize(),
+                    origin: CAM_ORIGIN,
+                    direction: (lower_left_corner + u * VIEWPORT_HORIZONTAL + v * VIEWPORT_VERTICAL - CAM_ORIGIN).normalize(),
                 };
-                let pixel_color = ray_color(&world, &ray, DIFFUSE_DEPTH, &mut rng);
+                let pixel_color = ray_color(&world, &ray, MAX_DEPTH, &mut rng);
 
                 sampled_color = sampled_color + pixel_color;
             }
-            sampled_color = sampled_color / SAMPLES as f32;
-            sampled_color.x = sampled_color.x.sqrt();
-            sampled_color.y = sampled_color.y.sqrt();
-            sampled_color.z = sampled_color.z.sqrt();
-            let r = (255.999 * sampled_color.x) as i32;
-            let g = (255.999 * sampled_color.y) as i32;
-            let b = (255.999 * sampled_color.z) as i32;
-
-            println!("{r} {g} {b}");
+            sampled_color = sampled_color / SAMPLES_PER_PIXEL as f32;
+            let rgb = translate_to_rgb_color(&mut sampled_color);
+            println!("{} {} {}",rgb.0, rgb.1, rgb.2 );
         }
     }
 }
